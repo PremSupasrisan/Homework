65c46a12c92ad5ec7a0acab887eb1fc02b03aaf9
PremSupasrisan <139094326+PremSupasrisan@users.noreply.github.com>
2023-07-24 10:06:14 +0700

    Add files via upload

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ int main()
 	int Score ;
 	cout << "Input C++ score : ";
 	cin  >> Score;
-	cout << "You "<<((Score >= 30 ) ? "pass with score " : "fail with score ")<<Score<<";"<<endl;
+	cout << "You "<<((Score >= 50 ) ? "pass with score " : "fail with score ")<<Score<<";"<<endl;
 	system ("pause");
 	return(0);
 }
