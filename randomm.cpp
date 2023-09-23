d06fc6e778319a8943c40254fcc39246b4d42245
PremSupasrisan <139094326+PremSupasrisan@users.noreply.github.com>
2023-09-24 00:40:07 +0700

    Add files via upload

---
+++ randomm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+int main()
+{
+	cout << "Element Value Histogram\n\n";
+	srand(time(NULL));
+	for (int i = 0; i < 10 ; i++)
+	{
+		int r = 1+rand() % 30;
+		cout << "	" << i << "	"<< r <<"	";
+		for ( int j = 0 ; j < r ; ++j)
+		{	cout << "*";
+		}
+		cout <<endl;
+	}
+	
+	
+	system("pause");
+	return 0;
+}
+
